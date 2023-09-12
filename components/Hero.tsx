ab2b7641f52fcc321e95c07c7ab96d3225e0c2bd
“mabruratiqi” <mabrurised@gmail.com>
2023-09-13 00:01:41 +0600

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from 'next/image';
+
+function Hero() {
+  return (
+    <div className='hero'>
+        <div className='flex-1 pt-36 padding-x'>
+            <h1 className='hero_title'>
+                Find, book or rent a car quickly and easily.
+            </h1>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
